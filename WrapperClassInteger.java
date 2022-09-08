91a0d264c655be8ddce6ee821360a0b40f2a5e69
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-09-09 00:27:53 +0530

    Create WrapperClassInteger.java

---
+++ WrapperClassInteger.java
@@ -0,0 +1,14 @@
+class AddNos
+{
+	public static void main(String [] args)
+	{
+		int a,b,c;
+		a=(int)Float.parseFloat(args[0]);
+		b=(int)Float.parseFloat(args[1]);
+		c=a+b;
+		System.out.println("Hello User");
+		System.out.println("first is "+a);
+		System.out.println("Secound is "+b);
+		System.out.println("sum is "+c);		
+	}
+}
