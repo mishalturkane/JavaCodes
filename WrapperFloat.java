e26a354fe14533bc7871e3bf10597a413e1f12d6
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-09-09 00:31:03 +0530

    Create WrapperFloat.java

---
+++ WrapperFloat.java
@@ -0,0 +1,14 @@
+class AddNos
+{
+	public static void main(String [] args)
+	{
+		float a,b,c;
+		a=Float.parseFloat(args[0]);
+		b=Float.parseFloat(args[1]);
+		c=a+b;
+		System.out.println("Hello User");
+		System.out.println("first is "+a);
+		System.out.println("Secound is "+b);
+		System.out.println("sum is "+c);		
+	}
+}
