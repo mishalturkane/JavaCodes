c6bfeaa6ce414a8dd4b5e2fb2f846e07157b2164
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-09-09 00:37:47 +0530

    Create DoubleWraperClass.java

---
+++ DoubleWraperClass.java
@@ -0,0 +1,14 @@
+class AddNos
+{
+	public static void main(String [] args)
+	{
+         double a,b,c;
+      	a=Double.parseDouble(args[0]);
+		b=Double.parseDouble(args[1]);
+		c=a+b;
+		System.out.println("Hello User");
+		System.out.println("first is "+a);
+		System.out.println("Secound is "+b);
+		System.out.println("sum is "+c);		
+	}
+}
