6609163942a93bcf690b445fac0825d34645386c
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-09-15 01:37:12 +0530

    Add files via upload
    charAt method from String class

---
+++ Test1.java
@@ -0,0 +1,12 @@
+class Test1
+{
+	public static void main(String [] args)
+	{
+		String s=new String("Mishal");
+		System.out.println(s.charAt(0));
+		System.out.println(s.charAt(1));
+		System.out.println(s.charAt(2));
+		System.out.println(s.charAt(3));
+	}
+
+}
