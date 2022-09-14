1f56d4cd0536bd82395dd58f1d8ee66c30e6ae3b
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-09-15 01:50:18 +0530

    Add files via upload
    equals method from String class

--- Test1.java
+++ Test1.java
@@ -3,10 +3,10 @@ class Test1
 	public static void main(String [] args)
 	{
 		String s=new String("Mishal");
-		System.out.println(s.charAt(0));
-		System.out.println(s.charAt(1));
-		System.out.println(s.charAt(2));
-		System.out.println(s.charAt(3));
+		String s1=new String("Mishal");
+		String s2=new String("MishaL");
+		System.out.println(s.equals(s1));
+		System.out.println(s.equals(s2));
 	}
 
 }
