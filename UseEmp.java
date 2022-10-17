1480ab8c91f5c7480e5d06dcb1c135a48919f5e9
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-10-17 19:18:23 +0530

    Add files via upload

--- UseEmp.java
+++ UseEmp.java
@@ -1,25 +1,9 @@
-package staticdemo;
+package staticblock;
 
 public class UseEmp {
     public static void main(String[] args) {
-        Emp e=new Emp(21,"Mishal");
-        e.show();
-        Emp f=new Emp(19,"Khusboo");
-        f.show();
-        e.showNextId();
-        {
-            Emp x=new Emp(15,"vilash");
-            Emp y=new Emp(20,"Muskan");
-
-            x.show();
-            y.show();
-            x.showNextId();
-
-            x=y=null;
-            System.gc();
-            System.runFinalization();
-        }
-        e.showNextId();
-
+        System.out.println(Emp.compname);
+        System.out.println(Emp.compname);
+        System.out.println(Emp.compname);
     }
 }
