8c777589b42c6c14bd07756085c7c9a98bc7e499
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-10-17 19:18:38 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,11 +1,11 @@
-class Test
-{  
-   
-    public static void main(String[] args)
-   {
-	
-	System.out.println("A is "a);
-       System.out.println("B is "+b);
-	 System.out.println("Sum is "+a+b);
-	}
+package staticblock;
+
+//1st soluton
+public class Test {
+    private  int a=10;
+
+    public static void main(String[] args) {
+        Test t=new Test();
+        System.out.println(t.a);
+    }
 }
