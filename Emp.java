2a76d0b85b340eb1b695c107853c04d79ba68037
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-10-20 23:12:32 +0530

    Add files via upload

--- Emp.java
+++ Emp.java
@@ -1,22 +1,20 @@
-package thisdemo;
+package inhdemo;
 
 public class Emp {
-    private int id;
     private String name;
     private double salary;
 
-    public Emp(int id, String name, double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    public void  setData(String name,double salary)
+    {
+        this.name=name;
+        this.salary=salary;
     }
 
+    public String getName() {
+        return name;
+    }
 
-    public void show()
-    {
-        System.out.println("id is:"+id);
-        System.out.println("Name is:"+name);
-        System.out.println("Salary is:"+salary);
+    public double getSalary() {
+        return salary;
     }
 }
-
