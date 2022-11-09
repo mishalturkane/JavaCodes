a562897ff5e5b981bd69d742d87b3065a4ec1cdc
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-09 19:14:11 +0530

    Add files via upload
    Interface related

---
+++ SampleImpl.java
@@ -0,0 +1,14 @@
+package interfacdemo4;
+
+public class SampleImpl implements  Sample{
+    public void m1()
+    {
+        System.out.println("In M1 of sampleImpl");
+    }
+
+   /*public  void m2()
+    {
+        System.out.println("in M2 of sampleImp");
+    }*/
+
+}
