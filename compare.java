509de073623ff0f77a48f942b1082b23d7773c1d
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-23 00:00:21 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,10 @@
+package Stringclass.java;
+
+public class compare {
+    public static void main(String[] args) {
+        String name="MISHAL";
+        String name2="MISHAL";
+        System.out.println(name.compareTo(name2));
+
+    }
+}
