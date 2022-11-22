40b95822408ff45b8e00705334358b7a770f1c8f
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-23 00:01:33 +0530

    Add files via upload

---
+++ Append.java
@@ -0,0 +1,8 @@
+package Stringbuffer;
+
+public class Append {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Mishal");
+        System.out.println(sb.append(" "+"Turkane "));
+    }
+}
