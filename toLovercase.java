513719f670a59650b0b9fd0b90bcbced60c01997
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-23 00:00:47 +0530

    Add files via upload

---
+++ toLovercase.java
@@ -0,0 +1,8 @@
+package Stringclass.java;
+
+public class toLovercase {
+    public static void main(String[] args) {
+        String name="Mishal";
+        System.out.println(name.toLowerCase());
+    }
+}
