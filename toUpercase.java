0e454124951d5d51a6860d8c83a1cd55350c12c9
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-23 00:01:01 +0530

    Add files via upload

---
+++ toUpercase.java
@@ -0,0 +1,8 @@
+package Stringclass.java;
+
+public class toUpercase {
+    public static void main(String[] args) {
+        String name="Mishal";
+        System.out.println(name.toUpperCase());
+    }
+}
