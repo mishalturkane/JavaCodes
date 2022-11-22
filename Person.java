9a39be09b80719a3f840f399f3b390e52dc9839c
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-22 18:33:18 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+///*
+//package excepmethods;
+//
+//public class Person {
+//
+//        private int age;
+//        private String name;
+//        public Person(int age,String name)
+//        {
+//            this.age=age;
+//            this.name=name;
+//        }
+//        @Override
+//    public String toStirng()
+//        {
+//            return age+","+name;
+//        }
+//
+//    }
+//
+//public class UsePerson
+//{
+//    public static void main(String[] args) {
+//        Person P=new Person(21,"Mishal");
+//        System.out.println(P);
+//    }
+//}
+//
+//*/
