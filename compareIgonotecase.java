af6e211668658f24e62d4503cf696322878690d7
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-11-23 00:00:34 +0530

    Add files via upload

---
+++ compareIgonotecase.java
@@ -0,0 +1,9 @@
+package Stringclass.java;
+
+public class compareIgonotecase {
+    public static void main(String[] args) {
+        String name="Mishal";
+        String name2="mishal";
+        System.out.println(name.compareToIgnoreCase(name2));
+    }
+}
