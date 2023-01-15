361ffbec58545ac56067621b618ecc9eb64e0c98
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2023-01-15 23:13:30 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,12 @@
+package basic;
+
+import java.util.Scanner;
+
+public class demo1 {
+    public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+        String a=kb.next();
+         String b=kb.next();
+        System.out.println(a+b);
+    }
+}
