4568e9772c599a58612f489f466e5db43fae785a
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2023-01-20 14:12:01 +0530

    Add files via upload

--- Num.java
+++ Num.java
@@ -1,19 +1,21 @@
-package argumentpassingdemo;
+package swapdemo;
 
 public class Num {
     private int a,b;
-    public void setNum(int i,int j)
-    {
+    public void setNum(int i ,int j){
         a=i;
         b=j;
     }
     public void showNum()
     {
-        System.out.println("a="+a+",b="+b);
+        System.out.println("a=:"+a+",b=:"+b);
     }
-    public void increment(Num P)
+    public void swap(Num P,Num Q)
     {
-        P.a++;
-        P.b++;
+        int x;
+        x=P.a;
+        P.a=Q.a;
+        Q.a=x;
+
     }
 }
--- UseNum.java
+++ UseNum.java
@@ -1,16 +1,21 @@
-package argumentpassingdemo;
+package swapdemo;
 
 public class UseNum {
     public static void main(String[] args) {
-        Num N=new Num();
+        Num N1=new Num();
+        Num N2=new Num();
+        N1.setNum(10,20);
+        N2.setNum(5,7);
+        System.out.println("Before swapping:");
 
-        N.setNum(10,20);
-        System.out.println("Before incremnt:");
-        N.showNum();
+        N1.showNum();
+        N2.showNum();
 
         Num Temp=new Num();
-        Temp.increment(N);
-        System.out.println("After increment:");
-        N.showNum();
+        Temp.swap(N1,N2);
+
+        System.out.println("after swapping:");
+        N1.showNum();
+        N2.showNum();
     }
 }
